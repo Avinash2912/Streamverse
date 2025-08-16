6f8e453f1bd611bdaf948a7a5f3e0e404af852ea
Avinash Jha <avinashjha19@outlook.com>
2025-08-17 03:48:21 +0530

    feat(express): set up Express server

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
-import dotenv from 'dotenv';
-import {connectToDatabase} from '../src/db/db_connection.js'; 
-
+import dotenv from "dotenv";
+import { connectToDatabase } from "../src/db/db_connection.js";
+import { app } from "../src/app.js";
+import { error } from "console";
 
 dotenv.config();
 
+connectToDatabase().then(() => {
+    app.on("error", (error) => {
+    console.error("Error connecting to the MONGODB:", error);
+    process.exit(1);
+});
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
 
-connectToDatabase();
+    app.on("error", (error) => {
+        console.error("Error in the Express app:", error);
+        process.exit(1);
+    });
+
+
+    
+}).catch((error) => {
+    console.error("Error connecting to the MONGODB:", error);
+    process.exit(1);
+});
+
+
