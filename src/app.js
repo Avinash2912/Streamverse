fe552fbd19db2c732289477fd8559193e29bdb7d
Avinash Jha <avinashjha19@outlook.com>
2025-08-18 02:18:42 +0530

    feat(api): implement ApiError and ApiResponse classes; add asyncHandler utility feat(cors): integrate CORS middleware and cookie-parser; enhance Express app setup chore(package): add cors and cookie-parser dependencies

--- src/app.js
+++ src/app.js
@@ -1,14 +1,32 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
+
+app.use(express.json({ limit: "10mb" }));
+
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
+app.use(express.static("public"));
+
+
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
 });
-
-
-
-
-
 
 export { app };
